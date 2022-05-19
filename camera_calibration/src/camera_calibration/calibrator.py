f40a05fc010df9f8438aad2c1d739c033c1a85a7
Kenzo Lobos Tsunekawa <kenzo.lobos@tier4.jp>
2022-05-20 08:34:39 +0900

    Fix the circle pattern board when it is vertically oriented (#5)
    * Fixed an issue in which the board pattern was handled incorrecly ending in a wrong calibration result
    
    * Increased readibility
    
    * Added an explanation to the fix

--- camera_calibration/src/camera_calibration/calibrator.py
+++ camera_calibration/src/camera_calibration/calibrator.py
@@ -249,6 +249,12 @@ def _get_circles(img, board, pattern):
             (ok, corners) = cv2.findCirclesGrid(
                 mono_arr, (board.n_rows, board.n_cols), flags=flag, blobDetector=detector)
 
+        # We need to swap the axes of the detections back to make it consistent
+        if ok:
+            corners_2d_array = corners.reshape((board.n_cols, board.n_rows, 2))
+            corners_transposed = np.transpose(corners_2d_array, (1, 0, 2))
+            corners = corners_transposed.reshape(-1, 1, 2)
+
     if ok:
         # Re-scaling results
         corners = corners * (1. / scale )
